81ed8f6c86c62f2f6df69ba81c0d8ec2d7a8ddb9
itonjaved <iton.javed@gmail.com>
2012-11-30 18:08:51 +0530

    modified Signed-off-by: itonjaved <iton.javed@gmail.com>

--- src/main/java/com/iton/gittest/HelloGit/MyVaadinApplication.java
+++ src/main/java/com/iton/gittest/HelloGit/MyVaadinApplication.java
@@ -37,7 +37,7 @@ public class MyVaadinApplication extends Application
         Button button = new Button("Click Me");
         button.addListener(new Button.ClickListener() {
             public void buttonClick(ClickEvent event) {
-                window.addComponent(new Label("Thank you for clicking"));
+                window.addComponent(new Label("Thank you for clicking one"));
             }
         });
         window.addComponent(button);
